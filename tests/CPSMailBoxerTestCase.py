592833e279fa06d147fe7de355f84cd3b6de4e6a
sfermigier <devnull@localhost>
2004-03-23 13:15:31 +0000

    Remove reference to customer project.

--- tests/CPSMailBoxerTestCase.py
+++ tests/CPSMailBoxerTestCase.py
@@ -3,11 +3,9 @@ from Products.CPSDefault.tests import CPSTestCase
 
 ZopeTestCase.installProduct('ZCatalog')
 ZopeTestCase.installProduct('MailBoxer')
-ZopeTestCase.installProduct('SuezCPS')
 ZopeTestCase.installProduct('CPSDocument')
 ZopeTestCase.installProduct('CPSSchemas')
 ZopeTestCase.installProduct('CPSMailBoxer')
-
 
 CPSTestCase.setupPortal()
 
