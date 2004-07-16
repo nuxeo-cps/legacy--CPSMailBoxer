477b0375bf791922ac839e4514431286d85091e0
epietrig <devnull@localhost>
2004-07-16 14:51:32 +0000

    now uses CPSInstaller defined a workflow for CPSMailArchive

--- Extensions/install.py
+++ Extensions/install.py
@@ -25,98 +25,274 @@
 # This should thus be done in your own installer
 
 from Products.CPSDefault.Installer import BaseInstaller
+from Products.CPSInstaller.CPSInstaller import CPSInstaller
+from Products.CMFCore.CMFCorePermissions import View, ModifyPortalContent
+from Products.CPSCore.CPSWorkflow import \
+     TRANSITION_INITIAL_PUBLISHING, TRANSITION_INITIAL_CREATE, \
+     TRANSITION_ALLOWSUB_CREATE, TRANSITION_ALLOWSUB_PUBLISHING, \
+     TRANSITION_BEHAVIOR_PUBLISHING, TRANSITION_BEHAVIOR_FREEZE, \
+     TRANSITION_BEHAVIOR_DELETE, TRANSITION_BEHAVIOR_MERGE, \
+     TRANSITION_ALLOWSUB_CHECKOUT, TRANSITION_INITIAL_CHECKOUT, \
+     TRANSITION_BEHAVIOR_CHECKOUT, TRANSITION_ALLOW_CHECKIN, \
+     TRANSITION_BEHAVIOR_CHECKIN, TRANSITION_ALLOWSUB_DELETE, \
+     TRANSITION_ALLOWSUB_MOVE, TRANSITION_ALLOWSUB_COPY
+from Products.DCWorkflow.Transitions import TRIGGER_USER_ACTION, \
+     TRIGGER_AUTOMATIC
+from Products.CPSMailBoxer.CPSMailBoxerPermissions import MailBoxerModerate
 
-class CPSMailBoxerInstaller(BaseInstaller):
-
-    SKINS = (('cpsmailboxer_default', 
-              'Products/CPSMailBoxer/skins/cpsmailboxer_default'),)
-
-    def install(self):
-        self.log("Starting CPSMailBoxer install")
-        self.product_name = 'CPSMailBoxer'
-        self.nmb = 'CPSMailBoxer'
-        self.nmbf = 'CPSMailBoxerFolder'
-        self.setupCpsDocumentDependantSkins(self.SKINS)
-        self.typestool = self.portal.portal_types
-        self.setupPortalTypes()
-        self.setupNMBMapperTool()
-        self.setupTranslations()
-        self.configurePlaceFullWorkflow()
-
-    def setupPortalTypes(self):
-        nmb = self.nmb
-        nmbf = self.nmbf
-        self.log(" Registering portal types: %s %s" % (nmb, nmbf))
-        ptypes_installed = self.typestool.objectIds()
-        if nmb in ptypes_installed:
-            self.typestool.manage_delObjects(nmb)
-            self.log("  Type %s Deleted" % (nmb,))
-        if nmbf in ptypes_installed:
-            self.typestool.manage_delObjects(nmbf)
-            self.log("  Type %s Deleted" % (nmbf,))
-        self.typestool.manage_addTypeInformation(
-            id=nmb,
-            add_meta_type='Factory-based Type Information',
-            typeinfo_name='CPSMailBoxer: CPSMailBoxer')
-        self.typestool[nmb].manage_changeProperties(
-            title='portal_type_CPSMailBoxer_title',
-            description='portal_type_CPSMailBoxer_description',
-            content_meta_type=nmb,
-            filter_content_types=1)
-        self.typestool.manage_addTypeInformation(
-            id=nmbf,
-            add_meta_type='Factory-based Type Information',
-            typeinfo_name='CPSMailBoxer: CPSMailBoxerFolder')
-        self.typestool[nmbf].manage_changeProperties(
-            title='portal_type_CPSMailBoxerFolder_title',
-            description='portal_type_CPSMailBoxerFolder_description',
-            content_meta_type=nmbf,
-            filter_content_types=1)
-        
-        self.log("Add our portal type id to allowed portal type of workspaces")
-
-    def setupNMBMapperTool(self):
-        self.log("Installing CPSMailBoxer Mapper Tool")
-        if self.portalHas('portal_mailboxermapper'):
-            self.logOK()
-        else:
-            self.log(" Creating CPSMailBoxerMapper Tool "
-                     "(portal_mailboxermapper)")
-            self.portal.manage_addProduct['CPSMailBoxer'].manage_addTool(
-                'MailBoxerMapper Tool')
-        self.log("Verifying Event service tool")
-        objs = self.portal.portal_eventservice.objectValues()
-        subscribers = []
-        for obj in objs:
-            subscribers.append(obj.subscriber)
-        if 'portal_mailboxermapper' in subscribers:
-            self.logOK()
-        else:
-            self.log(" Creating portal_mailboxermapper subscribers")
-            self.portal.portal_eventservice.manage_addSubscriber(
-                subscriber='portal_mailboxermapper',
-                action='nmbreg',
-                meta_type='CPSMailBoxer',
-                event_type='*',
-                notification_type='synchronous')
-
-    def configurePlaceFullWorkflow(self):
-        """
-        Configure the workspaces placefull workflow
-        """
-        self.log("Installing Workflow chain in placefull workflow "
-                 "configuration of root of Workspaces and sections")
-        wf_config = getattr(self.portal.workspaces, 
-                            '.cps_workflow_configuration')
-        wf_config.manage_addChain(portal_type=self.nmb, 
-                                  chain='workspace_folder_wf')
-        wf_config = getattr(self.portal.sections, 
-                            '.cps_workflow_configuration')
-        wf_config.manage_addChain(portal_type=self.nmb, 
-                                  chain='section_folder_wf')
-
-        
 def install(self):
-    installer = CPSMailBoxerInstaller(self)
-    installer.install()
+    
+    ##############################################
+    # Create the installer
+    ##############################################
+    installer = CPSInstaller(self, 'CPSMailBoxer')
+    installer.log("Starting CPSMailBoxer install")
+    nmb = 'CPSMailBoxer'
+    nmbf = 'CPSMailBoxerFolder'
+    nmba = 'CPSMailArchive'
+    typestool = installer.portal.portal_types
+
+    #################################################
+    # MailBoxer-specific roles and permissions
+    #################################################
+    installer.log("Checking specific roles and permissions")
+    perms = {
+        MailBoxerModerate:
+        ('Manager', 'Owner', 'WorkspaceManager', 'MailBoxerModerator',
+         'SectionManager', 'SectionReviewer',),
+        }
+    installer.verifyRoles(('MailBoxerModerator',))
+    installer.setupPortalPermissions(perms)
+
+    ##########################################
+    # SKINS
+    ##########################################
+    skins = {'cpsmailboxer_default':
+             'Products/CPSMailBoxer/skins/cpsmailboxer_default'}
+    installer.verifySkins(skins)
+    installer.resetSkinCache()
+
+    ##########################################
+    # PORTAL TYPES
+    ##########################################
+    installer.log("Checking portal types")
+    installer.log(" Registering portal types: %s %s" % (nmb, nmbf))
+    ptypes_installed = typestool.objectIds()
+    if nmb in ptypes_installed:
+        typestool.manage_delObjects(nmb)
+        installer.log("  Type %s Deleted" % (nmb,))
+    if nmbf in ptypes_installed:
+        typestool.manage_delObjects(nmbf)
+        installer.log("  Type %s Deleted" % (nmbf,))
+    typestool.manage_addTypeInformation(
+        id=nmb,
+        add_meta_type='Factory-based Type Information',
+        typeinfo_name='CPSMailBoxer: CPSMailBoxer')
+    typestool[nmb].manage_changeProperties(
+        title='portal_type_CPSMailBoxer_title',
+        description='portal_type_CPSMailBoxer_description',
+        content_meta_type=nmb,
+        filter_content_types=1)
+    typestool.manage_addTypeInformation(
+        id=nmbf,
+        add_meta_type='Factory-based Type Information',
+        typeinfo_name='CPSMailBoxer: CPSMailBoxerFolder')
+    typestool[nmbf].manage_changeProperties(
+        title='portal_type_CPSMailBoxerFolder_title',
+        description='portal_type_CPSMailBoxerFolder_description',
+        content_meta_type=nmbf,
+            filter_content_types=1)
+
+
+    installer.log("Checking Mapper Tool")
+    if installer.portalHas('portal_mailboxermapper'):
+        installer.logOK()
+    else:
+        installer.log(" Creating CPSMailBoxerMapper Tool "
+                 "(portal_mailboxermapper)")
+        installer.portal.manage_addProduct['CPSMailBoxer'].manage_addTool(
+            'MailBoxerMapper Tool')
+    installer.log("Verifying Event service tool")
+    objs = installer.portal.portal_eventservice.objectValues()
+    subscribers = []
+    for obj in objs:
+        subscribers.append(obj.subscriber)
+    if 'portal_mailboxermapper' in subscribers:
+        installer.logOK()
+    else:
+        installer.log(" Creating portal_mailboxermapper subscribers")
+        installer.portal.portal_eventservice.manage_addSubscriber(
+            subscriber='portal_mailboxermapper',
+            action='nmbreg',
+            meta_type='CPSMailBoxer',
+            event_type='*',
+            notification_type='synchronous')
+
+        installer.log("Checking workflows")
+
+    ##########################################
+    # WORKFLOW DEFINITION
+    ##########################################
+    # workflow for MailBoxer mail archives
+    wfdef = {'wfid': 'mail_archive_wf',
+             'permissions': (View, ModifyPortalContent),
+             'state_var': 'review_state'}
+    
+    wfstates = {
+        'created': {
+            'title': 'Created',
+            'transitions': ('auto_publish', 'auto_moderate'), #'auto_publish', 
+            'permissions': {View: ('Manager', 'Owner', 'WorkspaceManager',
+                                   'SectionManager', 'SectionReviewer',
+                                   'MailBoxerModerator'),
+                            ModifyPortalContent: ('Manager', 'Owner',
+                                                  'WorkspaceManager',
+                                                  'SectionManager',
+                                                  'SectionReviewer',
+                                                  'MailBoxerModerator')},
+            },
+        'pending': {
+            'title': 'Awaiting acceptance',
+            'transitions': ('publish', 'reject'),
+            'permissions': {View: ('Manager', 'Owner', 'WorkspaceManager',
+                                   'SectionManager', 'SectionReviewer',
+                                   'MailBoxerModerator'),
+                            ModifyPortalContent: ('Manager', 'Owner',
+                                                  'WorkspaceManager',
+                                                  'SectionManager',
+                                                  'SectionReviewer',
+                                                  'MailBoxerModerator')},
+            },
+        'published': {
+            'title': 'Public',
+            'transitions': (),
+            'permissions': {View: ('Manager', 'Owner', 'WorkspaceManager',
+                                   'WorkspaceMember', 'WorkspaceReader',
+                                   'SectionManager', 'SectionReviewer',
+                                   'SectionReader',
+                                   'MailBoxerModerator'),
+                            ModifyPortalContent: ('Manager',)},
+            },
+        'rejected': {
+            'title': 'Rejected',
+            'transitions': (),
+            'permissions': {View: ('Manager', 'Owner', 'WorkspaceManager',
+                                   'SectionManager', 'SectionReviewer',
+                                   'MailBoxerModerator'),
+                            ModifyPortalContent: ('Manager',)},
+            }
+        }
+    
+    wftransitions = {
+        'create': {
+            'title': 'Initial creation',
+            'new_state_id': 'created',
+            'transition_behavior': (TRANSITION_INITIAL_CREATE, ),
+            'clone_allowed_transitions': None,
+            'after_script_name': 'msg_edit',
+            'trigger_type': TRIGGER_USER_ACTION,
+            'props': {'guard_permissions': '',
+                      'guard_roles':'',
+                      'guard_expr':''},
+            },
+        'auto_publish': {
+            'title': 'No moderation, publishing',
+            'new_state_id': 'published',
+            'trigger_type': TRIGGER_AUTOMATIC,
+            'clone_allowed_transitions': None,
+            'after_script_name': 'post_publish',
+            'props': {'guard_permissions': '',
+                      'guard_roles': '',
+                      'guard_expr': 'python:container.getContent().moderation_mode == 0 or user.has_permission(\'MailBoxer Moderate\', container.getContent())'},
+            },
+        'auto_moderate': {
+            'title': 'Moderating',
+            'new_state_id': 'pending',
+            'trigger_type': TRIGGER_AUTOMATIC,
+            'clone_allowed_transitions': None,
+            'props': {'guard_permissions': '',
+                      'guard_roles': '',
+                      'guard_expr': 'python: container.getContent().moderation_mode == 1 and not user.has_permission(\'MailBoxer Moderate\', container.getContent())'},
+            },
+        'publish': {
+            'title': 'Publishing post',
+            'new_state_id': 'published',
+            'clone_allowed_transitions': None,
+            'trigger_type': TRIGGER_USER_ACTION,
+            'props': {'guard_permissions': 'MailBoxer Moderate',
+                      'guard_roles':'',
+                      'guard_expr':''},
+            },
+        'reject': {
+            'title': 'Rejecting post',
+            'new_state_id': 'rejected',
+            'clone_allowed_transitions': None,
+            'trigger_type': TRIGGER_USER_ACTION,
+            'props': {'guard_permissions': 'MailBoxer Moderate',
+                      'guard_roles':'',
+                      'guard_expr':''},
+            },
+        }
+    
+    wfscripts = {
+        'msg_edit': {
+        '_owner': None,
+        '_proxy_roles': ['Manager'],
+        'script': """\
+##parameters=state_change
+object = state_change.object
+subject = state_change.kwargs.get('subject', '')
+author = state_change.kwargs.get('author', '')
+msg = state_change.kwargs.get('message', '')
+kw = {'Title': subject,
+      'Description': msg,
+      'author': author,
+      'parent_id': pid}
+
+object.getEditableContent().edit(**kw)
+"""
+        },
+        }
+    
+    wfvariables = {
+        'subject': {
+            'description': 'post subject',
+            'default_expr': "python:state_change.kwargs.get('subject', '')",
+            'for_status': 1,
+            'update_always': 1,
+            },
+        'author': {
+            'description': 'post author',
+            'default_expr': "python:state_change.kwargs.get('author', '')",
+            'for_status': 1,
+            'update_always': 1,
+            },
+        'message': {
+            'description': 'post text',
+            'default_expr': "python:state_change.kwargs.get('message', '')",
+            'for_status': 1,
+            'update_always': 1,
+            },
+        }
+
+    installer.verifyWorkflow(wfdef, wfstates, wftransitions,
+                               wfscripts, wfvariables)
+    
+    ws_chains = {'CPSMailBoxer': 'workspace_folder_wf', 'CPSMailArchive': 'mail_archive_wf'}
+    se_chains = {'CPSMailBoxer': 'section_folder_wf', 'CPSMailArchive': 'mail_archive_wf'}
+    installer.verifyLocalWorkflowChains(installer.portal['workspaces'],
+                                        ws_chains)
+    installer.verifyLocalWorkflowChains(installer.portal['sections'],
+                                        se_chains)
+    
+    ##############################################
+    # i18n support
+    ##############################################
+    installer.setupTranslations()
+
+    ##############################################
+    # Finished!
+    ##############################################
+    installer.finalize()
+    installer.log("End of CPSMailBoxer install")
     return installer.logResult()
