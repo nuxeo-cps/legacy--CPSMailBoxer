a4d02a8d63f0c0fa2559bd3feac63e61bb59aaa4
epietrig <devnull@localhost>
2004-07-21 15:07:02 +0000

    scrpit for publishing selected pending messages

---
+++ skins/cpsmailboxer_default/cpsmailboxer_publish_msg.py
@@ -0,0 +1,12 @@
+##parameters=msg_paths=[],comment='',REQUEST=None
+# $Id$
+
+for msg_path in msg_paths:
+    msg = context.restrictedTraverse(context.getBaseUrl() + msg_path)
+    review_state = context.portal_workflow.getInfoFor(msg, 'review_state', 'nostate')
+    if review_state == 'pending':
+        context.portal_workflow.doActionFor(msg, 'publish', comment=comment)
+
+if REQUEST is not None:
+    url = "%s/cpsmailboxer_moderation_form?portal_status_message=nmb_messages_published" % context.absolute_url()
+    REQUEST.RESPONSE.redirect(url)
