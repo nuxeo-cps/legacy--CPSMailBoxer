f3a13b57d53868f821a10d840dfc1a3ce88bcc0b
epietrig <devnull@localhost>
2004-07-22 09:54:42 +0000

    fixed typo in PSM msgid

--- skins/cpsmailboxer_default/cpsmailboxer_publish_msg.py
+++ skins/cpsmailboxer_default/cpsmailboxer_publish_msg.py
@@ -8,5 +8,5 @@ for msg_path in msg_paths:
         context.portal_workflow.doActionFor(msg, 'publish', comment=comment)
 
 if REQUEST is not None:
-    url = "%s/cpsmailboxer_moderation_form?portal_status_message=nmb_messages_published" % context.absolute_url()
+    url = "%s/cpsmailboxer_moderation_form?portal_status_message=nmb_psm_messages_published" % context.absolute_url()
     REQUEST.RESPONSE.redirect(url)
--- skins/cpsmailboxer_default/cpsmailboxer_reject_msg.py
+++ skins/cpsmailboxer_default/cpsmailboxer_reject_msg.py
@@ -9,5 +9,5 @@ for msg_path in msg_paths:
         context.portal_workflow.doActionFor(msg, 'reject', comment=comment)
 
 if REQUEST is not None:
-    url = "%s/cpsmailboxer_moderation_form?portal_status_message=nmb_messages_rejected" % context.absolute_url()
+    url = "%s/cpsmailboxer_moderation_form?portal_status_message=nmb_psm_messages_rejected" % context.absolute_url()
     REQUEST.RESPONSE.redirect(url)
