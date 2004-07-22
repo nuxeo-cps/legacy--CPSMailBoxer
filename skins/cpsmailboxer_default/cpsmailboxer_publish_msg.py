d65c30d240992ec7ff60cc06358c130312d50c1d
epietrig <devnull@localhost>
2004-07-22 14:36:20 +0000

    call acceptMail method here rather than in an WF transition after_script

--- skins/cpsmailboxer_default/cpsmailboxer_publish_msg.py
+++ skins/cpsmailboxer_default/cpsmailboxer_publish_msg.py
@@ -1,11 +1,14 @@
 ##parameters=msg_paths=[],comment='',REQUEST=None
 # $Id$
 
+mb = context.getContent()
+
 for msg_path in msg_paths:
     msg = context.restrictedTraverse(context.getBaseUrl() + msg_path)
     review_state = context.portal_workflow.getInfoFor(msg, 'review_state', 'nostate')
     if review_state == 'pending':
         context.portal_workflow.doActionFor(msg, 'publish', comment=comment)
+        mb.acceptMail(msg.getContent())
 
 if REQUEST is not None:
     url = "%s/cpsmailboxer_moderation_form?portal_status_message=nmb_psm_messages_published" % context.absolute_url()
--- skins/cpsmailboxer_default/cpsmailboxer_reject_msg.py
+++ skins/cpsmailboxer_default/cpsmailboxer_reject_msg.py
@@ -1,12 +1,15 @@
 ##parameters=msg_paths=[],comment='',REQUEST=None
 # $Id$
 
+mb = context.getContent()
+
 for msg_path in msg_paths:
     msg = context.restrictedTraverse(context.getBaseUrl() + msg_path)
     review_state = context.portal_workflow.getInfoFor(msg, 'review_state', 'nostate')
     if review_state == 'pending':
-        context.getContent().notifyReject(msg, comment=comment)
+        mb.notifyReject(msg, comment=comment)
         context.portal_workflow.doActionFor(msg, 'reject', comment=comment)
+        mb.rejectMail(msg.getContent())
 
 if REQUEST is not None:
     url = "%s/cpsmailboxer_moderation_form?portal_status_message=nmb_psm_messages_rejected" % context.absolute_url()
