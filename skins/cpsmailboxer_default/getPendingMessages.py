ccde1afeb261e8dfdfb29808a9b5c435ac4afe6d
epietrig <devnull@localhost>
2004-07-22 08:07:20 +0000

    removed useless import

--- skins/cpsmailboxer_default/getPendingMessages.py
+++ skins/cpsmailboxer_default/getPendingMessages.py
@@ -2,8 +2,6 @@
 
 # $Id$
 
-from zLOG import LOG, DEBUG
-
 fp = context.portal_url.getRelativeUrl(context)
 
 return context.search(query={'modified_usage': 'range:min', 'portal_type': ['CPSMailArchive'], 'review_state': 'pending', 'folder_prefix': fp}, folder_prefix=fp)
