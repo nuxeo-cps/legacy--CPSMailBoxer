fb8d474b692adc4586ac967f818581c1a4170f14
epietrig <devnull@localhost>
2004-07-23 07:41:11 +0000

    pass msg proxy as parameter (required for deletino when no archive is stored)

--- skins/cpsmailboxer_default/cpsmailboxer_publish_msg.py
+++ skins/cpsmailboxer_default/cpsmailboxer_publish_msg.py
@@ -8,7 +8,7 @@ for msg_path in msg_paths:
     review_state = context.portal_workflow.getInfoFor(msg, 'review_state', 'nostate')
     if review_state == 'pending':
         context.portal_workflow.doActionFor(msg, 'publish', comment=comment)
-        mb.acceptMail(msg.getContent())
+        mb.acceptMail(msg.getContent(), msg)
 
 if REQUEST is not None:
     url = "%s/cpsmailboxer_moderation_form?portal_status_message=nmb_psm_messages_published" % context.absolute_url()
